68667134fa16e81f8973b6eaba2642ebcf38cc3b
MTH <thornvik.mans@gmail.com>
2017-11-18 19:35:31 +0100

    Get all three tables information into frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,24 +8,52 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   title = 'app';
-  results: string[];
+  users: any;
+  groups: any;
+  info: any;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-
+    const headers = new HttpHeaders().set('Authorization', 'Basic ' + btoa('admin:Horseseathay1'))
     // btoa() needs to be used since the Authorization header uses base64 encoding!
     // Without it, the request will fail with error code 401, unauthorized since it
     // cannot decode the request header.
     this.http.get(
       'http://localhost:8000/api/users/',
-      { headers: new HttpHeaders().set('Authorization', 'Basic ' + btoa('admin:Horseseathay1')) }
+      { headers: headers }
       ).subscribe(
         data => {
           console.log(data);
+          this.users = data;
       },
         err => {
-          console.log("ERROR RETREIVING USERS");
-      });
+          console.log(err);
+    });
+
+    this.http.get(
+      'http://localhost:8000/api/groups/',
+      { headers: headers }
+      ).subscribe(
+        data => {
+          console.log(data);
+          this.groups = data;
+      },
+        err => {
+          console.log(err);
+    });
+
+    this.http.get(
+      'http://localhost:8000/api/info/',
+      { headers: headers }
+      ).subscribe(
+        data => {
+          console.log(data);
+          this.info = data;
+      },
+        err => {
+          console.log(err);
+    });
+
   }
 }
