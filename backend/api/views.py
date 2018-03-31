8369a65f3a75b6654bdbe7f5004d4706f0d49744
MTH <thornvik.mans@gmail.com>
2018-03-31 13:43:27 +0200

    Fixed back and frontend dev server warnings

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 
-from rest_framework import viewsets
+from rest_framework import viewsets, pagination
 
 from api.models import Info
 from api.serializer import UserSerializer, GroupSerializer, InfoSerializer
@@ -9,6 +9,7 @@ from api.serializer import UserSerializer, GroupSerializer, InfoSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    pagination_class = pagination.LimitOffsetPagination
 # To specify specific permission and authentication classes for different
 # viewsets, uncomment the lines below and add the appropriate imports.
 #    permission_classes = (IsAdminUser, )
@@ -18,8 +19,10 @@ class UserViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    pagination_class = pagination.LimitOffsetPagination
 
 
 class InfoViewSet(viewsets.ModelViewSet):
     queryset = Info.objects.all()
     serializer_class = InfoSerializer
+    pagination_class = pagination.LimitOffsetPagination
---
+++ frontend/src/app/api-interface/result-set.interface.ts
@@ -0,0 +1,6 @@
+export interface ResultSet {
+  count: number;
+  next: string;
+  previous: string;
+  results: any[];
+}
