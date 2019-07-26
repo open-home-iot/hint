ae6c7b00fd6568f8fcdc61313924cc5f8337f359
MTH <thornvik.mans@gmail.com>
2019-07-26 22:42:44 +0200

    Added initial components to frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { AreaComponent } from './area/area.component';
+import { HumeComponent } from './hume/hume.component';
+import { DeviceComponent } from './device/device.component';
+import { EventComponent } from './event/event.component';
+import { HttpComponent } from './http/http.component';
+import { HeaderComponent } from './header/header.component';
+import { StartComponent } from './start/start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    AreaComponent,
+    HumeComponent,
+    DeviceComponent,
+    EventComponent,
+    HttpComponent,
+    HeaderComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/hume/hume.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hume',
+  templateUrl: './hume.component.html',
+  styleUrls: ['./hume.component.scss']
+})
+export class HumeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
