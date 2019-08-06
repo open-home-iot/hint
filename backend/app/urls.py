e5799fa2e93744d29c71e953cdbf162770d9e417
MTH <thornvik.mans@gmail.com>
2019-08-06 19:49:40 +0200

    Made Angular handle all URLs intended for front end

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,12 +1,11 @@
-from django.urls import path, re_path, include
+from django.urls import path, re_path
 
 from .views import AppView
 from .consumers import HumeConsumer
 
 
 urlpatterns = [
-    path("", AppView.as_view(), name='index'),
-    path("auth/", include('django.contrib.auth.urls')),
+    path("", AppView.as_view(), name='index')
 ]
 
 websocket_urlpatterns = [
--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from django.contrib import admin
 
 from backend.app import urls as app_urls
 from backend.api import urls as api_urls
@@ -7,10 +6,13 @@ from backend.app.urls import websocket_urlpatterns as app_ws_urlpatterns
 
 
 urlpatterns = [
-    path("", include(app_urls)),
-    path("app/", include(app_urls)),
     path("api/", include(api_urls)),
-    path("admin", admin.site.urls),
+
+    # Empty path will forward to front end Angular application.
+    path("", include(app_urls)),
+    # Catch all is forwarded to the Angular front end application, event 404's
+    # are handled by the Angular application.
+    path("<path:url>", include(app_urls)),
 ]
 
 # TODO fill in
