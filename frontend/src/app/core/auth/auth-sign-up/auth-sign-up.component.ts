2d66dd28b6865aa979752491d10c8df0d5a2f70a
MTH <thornvik.mans@gmail.com>
2020-09-28 22:05:55 +0200

    Add dropdown under account icon for account link and logout. Add login to start page dependent on authentication status.

---
+++ frontend/src/app/core/auth/auth-sign-up/auth-sign-up.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-sign-up',
+  templateUrl: './auth-sign-up.component.html',
+  styleUrls: ['./auth-sign-up.component.scss']
+})
+export class AuthSignUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
