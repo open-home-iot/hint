888751e35f5001555a023dd339cd65295234852a
MTH <thornvik.mans@gmail.com>
2020-10-04 19:45:14 +0200

    Replace user admin forms with custom ones to support creating and updating custom user instances through the Django admin.

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,20 +1,78 @@
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 
-class CustomUserAdmin(UserAdmin):
-    ordering = ('email',)
-    list_display = ('email',)
-    fieldsets = (
-        (None, {'fields': ('email', 'password', 'first_name',
-                           'last_name', 'is_active', 'is_staff',
-                           'is_superuser', 'groups')}),
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Password confirmation', widget=forms.PasswordInput
     )
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'password', 'is_active')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+
+    ordering = ('email',)
+    list_display = ('email', 'first_name', 'last_name', 'is_active',
+                    'is_staff', 'is_superuser', 'last_login')
+
     add_fieldsets = (
-        (None, {'fields': ('email', 'password', 'first_name',
-                           'last_name', 'is_active', 'is_staff',
-                           'is_superuser', 'groups')}),
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1',
+                       'password2'),
+        }),
+    )
+
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'first_name', 'last_name', 'password')
+        }),
     )
 
 
