8a22ccc63b9e43936c3073817d6efc5349036fca
MTH <thornvik.mans@gmail.com>
2020-11-05 19:00:38 +0100

    Update API path

---
+++ backend/hume/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from backend.hume.views import Humes, HumeFind, HumeAssociate, \
+                               HumeConfirmPairing, HomeHumes
+
+
+urlpatterns = [
+    path("", Humes.as_view()),
+    path("<str:hume_uuid>", HumeFind.as_view()),
+    path("<int:hume_id>/associate", HumeAssociate.as_view()),
+    path("<int:hume_id>/confirm-pairing", HumeConfirmPairing.as_view()),
+]
