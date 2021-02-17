ff36fbf52c836c4cc67b1b64fd20f88d0ca9babf
Måns Thörnvik <thornvik.mans@gmail.com>
2021-02-17 13:36:04 +0100

    Ignore wildcard import linter warnings

--- backend/settings/__init__.py
+++ backend/settings/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
 if sys.argv[1] == "test":
-    from backend.settings.test import *
+    from backend.settings.test import *  # noqa
 else:
     from backend.settings.local import *  # noqa
