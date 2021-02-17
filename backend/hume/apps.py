6c5487fa78e0a0e6a70df8b572980b0b97fa866d
Måns Thörnvik <thornvik.mans@gmail.com>
2021-02-17 11:20:08 +0100

    Update requirements to include pylint. Build workflow updated to install project dependencies

--- backend/hume/apps.py
+++ backend/hume/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class HumeConfig(AppConfig):
     # pylint: disable=missing-class-docstring
     name = 'backend.hume'
-
-#    def ready(self):
-        # Used to register signals for this application, in this case post_delete for users.
-#        import backend.hume.signals  # noqa
