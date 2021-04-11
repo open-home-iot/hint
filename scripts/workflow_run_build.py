f116475741f0b06824c6de875325c180f6ced518
Måns Thörnvik <thornvik.mans@gmail.com>
2021-04-11 22:10:54 +0200

    Update mac start dev server script. Add workflow dispatch script

---
+++ scripts/workflow_run_build.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Must define $GITHUB_USERNAME and $HINT_WORKFLOW_TOKEN. The token should be a GitHub PAT with repo"
+                    "access."
+    )
+    parser.add_argument(
+        "ref",
+        type=str,
+        help="Branch or tag name to run the workflow on."
+    )
+    return parser.parse_args()
+
+
+args = parse_args()
+gh_username = os.environ.get("GITHUB_USERNAME")
+hint_wf_token = os.environ.get("HINT_WORKFLOW_TOKEN")
+if gh_username is None or hint_wf_token is None:
+    raise SystemError("Missing $GITHUB_USERNAME or $HINT_WORKFLOW_TOKEN")
+
+subprocess.run(["curl", "-X", "POST",
+                "https://api.github.com/repos/megacorpincorporated/hint/actions/workflows/5880406/dispatches",
+                "-u", f"{gh_username}:{hint_wf_token}", "-d", f"{{\"ref\": \"{args.ref}\"}}"])
