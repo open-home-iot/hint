2ac8a55e125bdc92e790dd70dbc278eb342e39a0
Måns Thörnvik <thornvik.mans@gmail.com>
2021-09-27 01:05:41 +0200

    Handle a favourited home being deleted

--- frontend/src/app/features/home/pages/home-overview/home-overview.component.ts
+++ frontend/src/app/features/home/pages/home-overview/home-overview.component.ts
@@ -38,11 +38,17 @@ export class HomeOverviewComponent implements OnInit {
     const SELECTED_HOME_ID = localStorage.getItem(LS_KEY_SELECTED_HOME_ID);
     if (SELECTED_HOME_ID !== null) {
       this.selectedHome = this.homes.get(Number(SELECTED_HOME_ID));
-    } else {
+    }
+
+    // A previous selected home may have been deleted or a home favourite may
+    // not yet have been selected.
+    if (this.selectedHome === undefined) {
+      localStorage.removeItem(LS_KEY_SELECTED_HOME_ID);
       const ITERATOR = this.homes.keys();
       const KEY = ITERATOR.next();
       if (KEY.value !== undefined) {
         this.selectedHome = this.homes.get(KEY.value);
+        localStorage.setItem(LS_KEY_SELECTED_HOME_ID, KEY.value);
       }
     }
   }
