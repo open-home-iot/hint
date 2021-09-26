825ad2dd38aa44dc68291a53bb765838182a158c
Måns Thörnvik <thornvik.mans@gmail.com>
2021-09-26 23:52:38 +0200

    Make add home button plus sign configurable

--- frontend/src/app/features/home/home-add/home-add.component.ts
+++ frontend/src/app/features/home/home-add/home-add.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { HomeService, Home } from '../home.service';
