b053601defb61cab55e07136f668cd6e1258f532
Måns Thörnvik <thornvik.mans@gmail.com>
2021-12-23 22:36:41 +0100

    Add home edit name and delete button, and list humes

--- frontend/src/app/features/home/pages/home-edit/home-edit.component.ts
+++ frontend/src/app/features/home/pages/home-edit/home-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Home, HomeService} from '../../home.service';
+import {Hume, HumeService} from '../../../hume/hume.service';
 
 @Component({
   selector: 'app-home-edit',
@@ -12,14 +13,20 @@ export class HomeEditComponent implements OnInit {
   home: Home;
   // To avoid string interpolation error on page load.
   homeName: string = "";
+  humes: Hume[];
 
   constructor(private route: ActivatedRoute,
-              private homeService: HomeService) { }
+              private homeService: HomeService,
+              private humeService: HumeService) { }
 
   ngOnInit(): void {
     this.homeService.getHome(Number(this.route.snapshot.params['id']))
       .then(this.onGetHome.bind(this))
       .catch(this.onGetHomeFailed);
+
+    this.humeService.getHomeHumes(Number(this.route.snapshot.params['id']))
+      .then(this.onGetHomeHumes.bind(this))
+      .catch(this.onGetHomeHumesFailed);
   }
 
   private onGetHome(home: Home) {
@@ -28,6 +35,14 @@ export class HomeEditComponent implements OnInit {
   }
 
   private onGetHomeFailed(error) {
+    console.error(error);
+  }
+
+  private onGetHomeHumes(humes: Hume[]) {
+    this.humes = humes;
+  }
+
+  private onGetHomeHumesFailed(error) {
     console.error(error);
   }
 }
