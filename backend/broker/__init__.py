8fe1e2a7c707e48463430d2d1035a661c25131e9
Måns Thörnvik <thornvik.mans@gmail.com>
2021-12-26 16:04:55 +0100

    Add testability to producer mod

--- backend/broker/__init__.py
+++ backend/broker/__init__.py
@@ -1,1 +1,7 @@
+import sys
+
 from .defs import MessageType  # noqa
+
+if len(sys.argv) > 1 and sys.argv[1] == "test":
+    from backend.broker import producer
+    producer._producer = producer.FakeProducer()
--- backend/hume/signal_handlers.py
+++ backend/hume/signal_handlers.py
@@ -3,12 +3,16 @@ from django.db.models.signals import post_delete
 from backend.hume.models import Hume
 from backend.user.models import User
 
+from backend.broker import producer
+
 
 @receiver(post_delete, sender=Hume)
-def on_delete(instance, **kwargs):
+def on_delete(instance: Hume, **kwargs):
     """
     Delete a deleted Hume's user instance. Cannot be done with cascade as that
     would require reversing the relation between the Hume and User models.
     """
     hume_user = User.objects.get(id=instance.hume_user.id)
     hume_user.delete()
+
+    producer.unpair(instance.uuid)
